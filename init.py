22e2605993462c184ef202ddfceaf7fe8293d429
Sam Barratt <samathy@sbarratt.co.uk>
2017-03-18 23:02:53 +0000

    Initial commit. Basic sockets

---
+++ init.py
@@ -0,0 +1,15 @@
+import socket
+
+
+sock = socket.socket()
+host = "localhost"
+port = 1337
+
+sock.bind((host,port))
+
+print("listening")
+sock.listen()
+
+while(1):
+    c, addr = sock.accept()
+    print ("Got connection from " + addr)
