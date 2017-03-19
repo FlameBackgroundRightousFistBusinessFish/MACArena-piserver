9d82774481eba06efa6e62b20963aeda09dde390
lex <lexicobob@gmail.com>
2017-03-19 01:52:39 +0000

    save connections to list

--- init.py
+++ init.py
@@ -1,6 +1,5 @@
 import socket
 
-
 sock = socket.socket()
 host = "localhost"
 port = 1337
@@ -8,11 +7,23 @@ port = 1337
 sock.bind((host,port))
 
 print("listening")
-sock.listen()
+sock.listen(5)
+
+connections []
+macs []
 
 while(1):
     c, addr = sock.accept()
     print ("Got connection from " + str(addr))
-
-    print(c.recv(2048))
-
+    mac = c.recv(2048)
+	connections.append(c)
+	mac.append(c)
+	
+	if len(connections) > 1 :
+		#ready to start a fight
+		#play some sound 
+		#print fight beginning
+		#create player 1 monster
+		#create player 2 monster
+		#fight
+	
